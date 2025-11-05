e2a91d848593467f235d6e60ab20ece3694d16cd
Jack Galligan <jack@Jacktop.localdomain>
2025-11-05 01:10:30 -0500

    Fixed test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,5 +4,5 @@ def test_addition():
 def test_subtraction():
     assert 5 - 2 == 3
 
-def test_failure_example():
-    assert 2 * 2 == 5  # This will fail intentionally
+def test_success_example():
+    assert 2 * 2 == 4  # This will succeed now
